05a665c392d00e67ae09dd4e167f31247a6a6710
Tan Nguyenv <tan.nguyenv@titancorpvn.com>
2019-06-25 11:02:34 +0700

    connect mongodb

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 4000;
+const mongoose = require('mongoose');
 
 app.use(cors());
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/mern-videos', { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
